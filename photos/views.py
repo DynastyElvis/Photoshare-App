fbe3116a2c5b76589383c300b0c84f5443d90154
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-09 16:46:16 +0300

    ft: views logic update

---
+++ photos/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .models import Category, Photo, Location
+
+
+# def gallery(request):#, category_id):
+#     category = request.GET.get('category')
+    
+
+def gallery(request):
+    category = request.GET.get('category')
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name = category)
+        
+    categories = Category.objects.all()   
+    locations = Location.objects.all()  
+    
+    
+    context = {'categories': categories, 'photos':photos, 'locations': locations}
+    
+    return render(request,'photos/gallery.html', context)
+
+# def gallery(request):#, category_id):
+#     category = request.GET.get('category')
+    
+    
+#     if category == 'None':
+#         photos = Photo.objects.all()
+
+
+#     else:
+#         photos = Photo.objects.filter(category__name=category)
+
+    
+#     categories = Category.objects.all()
+#     photos = Photo.objects.all()
+    
+#     context = {'categories': categories, 'photos': photos}
+#     return render(request, 'photos/gallery.html', context)
+
+
