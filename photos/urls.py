6acadeb9279f4bc3a76afcf46c6edd247e1f88e1
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-09 16:54:21 +0300

    ft: urlpatterns

--- photos/urls.py
+++ photos/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('',views.gallery,name='gallery'),
+    path('location/',views.location,name='location'),
+    path('photo/<str:pk>add/',views.viewPhoto,name='photo'),
+    path('add/',views.addPhoto,name='add'),
+]
