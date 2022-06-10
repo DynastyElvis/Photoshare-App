f9ba989aa433c42e309e1afff11fcb9faff0bc82
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-10 13:35:19 +0300

    ft: heroku

--- photobooth/settings.py
+++ photobooth/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = 'django-insecure-l4lr=ogt)-!cot2(mv#0#0$e6=2_&(%3+v-rp*jca4!hfbu&-%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://elvis-gallery.herokuapp.com', '127.0.0.1']
+# ALLOWED_HOSTS = ['https://elvis-gallery.herokuapp.com', '127.0.0.1']
 
-
+ALLOWED_HOSTS = ['*',]
 # Application definition
 
 INSTALLED_APPS = [
